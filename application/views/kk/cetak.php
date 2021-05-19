a3781c207c602e4f201d7541b4e589b02f13305b
satriadinata <satriadinatazr@gmail.com>
2021-05-19 09:22:27 +0700

    Update cetak.php

--- application/views/kk/cetak.php
+++ application/views/kk/cetak.php
@@ -7,7 +7,7 @@
 <style type="text/css">
 	body{
 		font-family: Arial, sans-serif;
-		font-size: 14px;
+		font-size: 12px;
 	}
 	table{
 		border-collapse: collapse;
@@ -16,7 +16,7 @@
 		list-style-type: none;
 	}
 	.table td, .table th{
-		padding: 5px;
+		padding: 1px;
 		border-style: solid;
 		border-width: 2px;
 		border-color: black;
@@ -37,14 +37,14 @@
 				<td>Nama Kepala Keluarga</td>
 				<td style="padding-left: 8px;padding-right: 8px;" > : </td>
 				<td><?php echo $kepala['nama_warga']; ?></td>
-			
+
 
 			</tr>
 			<tr>
 				<td>Alamat</td>
 				<td style="padding-left: 8px;padding-right: 8px;" > : </td>
 				<td><?php echo $calon['alamat_keluarga'];?></td>
-		
+
 
 			</tr>
 			<tr>
@@ -57,7 +57,7 @@
 				<td>Desa/Kelurahan</td>
 				<td style="padding-left: 8px;padding-right: 8px;" > : </td>
 				<td><?php echo $calon['desa_kelurahan_keluarga']; ?></td>
-		
+
 			</tr>
 		</table>
 
@@ -88,31 +88,31 @@
 			</tr>
 		</table>
 	</div>
-	<div class="body" style="">
-	<table style="width: 100%;" class="table table-bordered">
-		<thead>
-			<th>No</th>
-			<th>Nama Lengkap</th>
-			<th>NIK</th>
-			<th>Jenis Kelamin</th>
-			<th>Tempat Lahir</th>
-			<th>Tanggal Lahir</th>
-			<th>Agama</th>
-			<th>Pendidikan</th>
-			<th>Jenis Pekerjaan</th>
-		</thead>
-		<tbody>
-			<tr style="text-align: center;background:rgba(0,0,0,.1);" >
-				<td></td>
-				<td>(1)</td>
-				<td>(2)</td>
-				<td>(3)</td>
-				<td>(4)</td>
-				<td>(5)</td>
-				<td>(6)</td>
-				<td>(7)</td>
-				<td>(8)</td>
-			</tr>
+	<div class="body" style="height: 300;">
+		<table style="width: 100%;" class="table table-bordered">
+			<thead>
+				<th>No</th>
+				<th>Nama Lengkap</th>
+				<th>NIK</th>
+				<th>Jenis Kelamin</th>
+				<th>Tempat Lahir</th>
+				<th>Tanggal Lahir</th>
+				<th>Agama</th>
+				<th>Pendidikan</th>
+				<th>Jenis Pekerjaan</th>
+			</thead>
+			<tbody>
+				<tr style="text-align: center;background:rgba(0,0,0,.1);" >
+					<td></td>
+					<td>(1)</td>
+					<td>(2)</td>
+					<td>(3)</td>
+					<td>(4)</td>
+					<td>(5)</td>
+					<td>(6)</td>
+					<td>(7)</td>
+					<td>(8)</td>
+				</tr>
 			<!-- <tr>
 				<td><?php $no=1; echo $no; ?></td>
 				<td><?php echo $kepala['nama_warga']; ?></td>
@@ -125,23 +125,23 @@
 				<td><?php echo $kepala['pekerjaan_warga']; $no++;?></td>
 			</tr> -->
 			<?php $no=1;foreach ($anggota as $key => $value) :?>
-				<tr>
-					<td><?php echo $no; ?></td>
-					<td><?php echo $value->nama_warga?></td>
-					<td><?php echo $value->nik_warga?></td>
-					<td><?php echo $value->jenis_kelamin_warga?></td>
-					<td><?php echo $value->tempat_lahir_warga?></td>
-					<td><?php echo $value->tanggal_lahir_warga?></td>
-					<td><?php echo $value->agama_warga?></td>
-					<td><?php echo $value->pendidikan_terakhir_warga?></td>
-					<td><?php echo $value->pekerjaan_warga?></td>
-				</tr>
-				<?php $no++; endforeach ?>
-			</tbody>
-		</table>
-		<br>
-		<br>
-		<table style="width: 100%;" class="table table-bordered">
+			<tr>
+				<td><?php echo $no; ?></td>
+				<td><?php echo $value->nama_warga?></td>
+				<td><?php echo $value->nik_warga?></td>
+				<td><?php echo $value->jenis_kelamin_warga?></td>
+				<td><?php echo $value->tempat_lahir_warga?></td>
+				<td><?php echo $value->tanggal_lahir_warga?></td>
+				<td><?php echo $value->agama_warga?></td>
+				<td><?php echo $value->pendidikan_terakhir_warga?></td>
+				<td><?php echo $value->pekerjaan_warga?></td>
+			</tr>
+			<?php $no++; endforeach ?>
+		</tbody>
+	</table>
+	<br>
+	<br>
+	<table style="width: 100%;" class="table table-bordered">
 		<thead>
 			<th rowspan="2" >No</th>
 			<th rowspan="2" >Status Pernikahan</th>
@@ -182,28 +182,28 @@
 				<td><?php echo $kepala['pendidikan_terakhir_warga']; $no++;?></td>
 			</tr> -->
 			<?php $no=1;foreach ($anggota as $key => $value) :?>
-				<tr>
-					<td><?php echo $no; ?></td>
-					<td><?php echo $value->status_perkawinan_warga?></td>
-					<td><?php echo $value->shdk?></td>
-					<td><?php echo ($value->negara_warga=='Indonesia'||$value->negara_warga=='indonesia'||$value->negara_warga=='INDONESIA') ? 'WNI' :'WNA' ?></td>
-					<td><?php echo $value->no_paspor?></td>
-					<td><?php echo $value->no_kit_as_ap?></td>
-					<td><?php echo $value->ayah?></td>
-					<td><?php echo $value->ibu?></td>
-				</tr>
-				<?php $no++; endforeach ?>
-			</tbody>
-		</table>
-		</div>
-		<div style="position: relative;width: 100%;">
-			<div style="position: absolute;right: -760;text-align: center;" >
-				<p>KEPALA KELUARGA</p>
-				<br>
-				<br>
-				<h4 style="text-decoration: underline;margin-bottom: -6px;" ><?php echo $kepala['nama_warga']; ?></h4>
-				<p>Tanda Tangan/Cap Jempol</p>
-			</div>
-		</div>
-	</body>
-	</html>
+			<tr>
+				<td><?php echo $no; ?></td>
+				<td><?php echo $value->status_perkawinan_warga?></td>
+				<td><?php echo $value->shdk?></td>
+				<td><?php echo ($value->negara_warga=='Indonesia'||$value->negara_warga=='indonesia'||$value->negara_warga=='INDONESIA') ? 'WNI' :'WNA' ?></td>
+				<td><?php echo $value->no_paspor?></td>
+				<td><?php echo $value->no_kit_as_ap?></td>
+				<td><?php echo $value->ayah?></td>
+				<td><?php echo $value->ibu?></td>
+			</tr>
+			<?php $no++; endforeach ?>
+		</tbody>
+	</table>
+</div>
+<div style="position: relative;width: 100%;">
+	<div style="position: absolute;right: -760;text-align: center;" >
+		<p>KEPALA KELUARGA</p>
+		<br>
+		<br>
+		<h4 style="text-decoration: underline;margin-bottom: -6px;" ><?php echo $kepala['nama_warga']; ?></h4>
+		<p>Tanda Tangan/Cap Jempol</p>
+	</div>
+</div>
+</body>
+</html>
